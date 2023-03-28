03c7aca4d156627bd62249477bbb4e1472048631
Shubham <shubhamv2010@gmail.com>
2023-03-28 13:01:11 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListingsPageComponent } from './MyComponents/listings-page/listings-page.component';
+import { ListingDetailComponent } from './MyComponents/listing-detail/listing-detail.component';
+import { ContactPageComponent } from './MyComponents/contact-page/contact-page.component';
+import { EditListingPageComponent } from './MyComponents/edit-listing-page/edit-listing-page.component';
+import { MyListingsPageComponent } from './MyComponents/my-listings-page/my-listings-page.component';
+import { NewListingPageComponent } from './MyComponents/new-listing-page/new-listing-page.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'listings', pathMatch: 'full' },
+  { path: 'listings', component: ListingsPageComponent, pathMatch: 'full' },
+  { path: 'listings/:id', component: ListingDetailComponent },
+  { path: 'contact/:id', component: ContactPageComponent },
+  { path: 'edit-listing/:id', component: EditListingPageComponent },
+  { path: 'my-listings', component: MyListingsPageComponent },
+  { path: 'new-listing', component: NewListingPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListingsPageComponent } from './MyComponents/listings-page/listings-page.component';
+import { ListingDetailComponent } from './MyComponents/listing-detail/listing-detail.component';
+import { ContactPageComponent } from './MyComponents/contact-page/contact-page.component';
+import { MyListingsPageComponent } from './MyComponents/my-listings-page/my-listings-page.component';
+import { NewListingPageComponent } from './MyComponents/new-listing-page/new-listing-page.component';
+import { EditListingPageComponent } from './MyComponents/edit-listing-page/edit-listing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListingsPageComponent,
+    ListingDetailComponent,
+    ContactPageComponent,
+    MyListingsPageComponent,
+    NewListingPageComponent,
+    EditListingPageComponent
   ],
   imports: [
     BrowserModule,
